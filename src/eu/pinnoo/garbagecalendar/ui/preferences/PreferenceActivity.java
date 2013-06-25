881133c399f4de8511982b34246d5c9b96a01caf
Wouter Pinnoo <pinnoo.wouter@gmail.com>
2013-06-25 02:05:53 +0200

    removed old menu items, made notifications checkbox work

--- src/eu/pinnoo/garbagecalendar/ui/preferences/PreferenceActivity.java
+++ src/eu/pinnoo/garbagecalendar/ui/preferences/PreferenceActivity.java
@@ -1,6 +1,10 @@
 package eu.pinnoo.garbagecalendar.ui.preferences;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
@@ -10,14 +14,18 @@ import com.google.analytics.tracking.android.EasyTracker;
 import com.google.analytics.tracking.android.GoogleAnalytics;
 import eu.pinnoo.garbagecalendar.R;
 import eu.pinnoo.garbagecalendar.data.Address;
+import eu.pinnoo.garbagecalendar.data.Collection;
+import eu.pinnoo.garbagecalendar.data.CollectionsData;
 import eu.pinnoo.garbagecalendar.data.LocalConstants;
 import eu.pinnoo.garbagecalendar.data.UserData;
+import eu.pinnoo.garbagecalendar.receivers.TrashDayReceiver;
+import java.util.Calendar;
 
 /**
  *
  * @author Wouter Pinnoo <pinnoo.wouter@gmail.com>
  */
-public class PreferenceActivity extends Activity {
+public class PreferenceActivity extends Activity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +60,28 @@ public class PreferenceActivity extends Activity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals("notif_pref")) {
+            AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+            int i = 0;
+            for (Collection col : CollectionsData.getInstance().getCollections()) {
+                Intent intent = new Intent(getBaseContext(), TrashDayReceiver.class);
+                intent.putExtra(LocalConstants.NOTIF_INTENT_COL, col);
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(getBaseContext(), i++, intent, 0);
+
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(col.getDate());
+                calendar.add(Calendar.HOUR, -4);
+
+                if (sharedPreferences.getBoolean("notif_pref", false)) {
+                    alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+                } else {
+                    alarmManager.cancel(pendingIntent);
+                }
+            }
         }
     }
 
