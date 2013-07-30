cfb45ab502cce0cb2623e77eeb81b5ff973b9d6d
Wouter Pinnoo <pinnoo.wouter@gmail.com>
2013-07-30 16:36:00 +0200

    removed more code for notifications functionality

--- src/eu/pinnoo/garbagecalendar/ui/preferences/PreferenceActivity.java
+++ src/eu/pinnoo/garbagecalendar/ui/preferences/PreferenceActivity.java
@@ -1,9 +1,6 @@
 package eu.pinnoo.garbagecalendar.ui.preferences;
 
 import android.app.Activity;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
@@ -14,12 +11,8 @@ import com.google.analytics.tracking.android.EasyTracker;
 import com.google.analytics.tracking.android.GoogleAnalytics;
 import eu.pinnoo.garbagecalendar.R;
 import eu.pinnoo.garbagecalendar.data.Address;
-import eu.pinnoo.garbagecalendar.data.Collection;
-import eu.pinnoo.garbagecalendar.data.CollectionsData;
 import eu.pinnoo.garbagecalendar.data.LocalConstants;
 import eu.pinnoo.garbagecalendar.data.UserData;
-import eu.pinnoo.garbagecalendar.receivers.TrashDayReceiver;
-import java.util.Calendar;
 
 /**
  *
@@ -64,27 +57,9 @@ public class PreferenceActivity extends Activity implements SharedPreferences.On
     }
 
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if (key.equals("notif_pref")) {
-            AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-            int i = 0;
-            for (Collection col : CollectionsData.getInstance().getCollections()) {
-                Intent intent = new Intent(getBaseContext(), TrashDayReceiver.class);
-                intent.putExtra(LocalConstants.NOTIF_INTENT_COL, col);
-                PendingIntent pendingIntent = PendingIntent.getBroadcast(getBaseContext(), i++, intent, 0);
-
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTime(col.getDate());
-                calendar.add(Calendar.HOUR, -4);
-
-                if (sharedPreferences.getBoolean("notif_pref", false)) {
-                    alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-                } else {
-                    alarmManager.cancel(pendingIntent);
-                }
-            }
-        }
+        
     }
-
+    
     public class Preferences extends PreferenceFragment {
 
         @Override
@@ -105,9 +80,9 @@ public class PreferenceActivity extends Activity implements SharedPreferences.On
             Preference pref = (Preference) findPreference("address");
             Address address = UserData.getInstance().getAddress();
             if (address == null || address.getCode() == null || address.getCode().isEmpty()) {
-                pref.setSummary("No address set yet.");
+                pref.setSummary(getString(R.string.noAddressSet));
             } else {
-                pref.setSummary("Set on " + address.getStreetname() + ", " + address.getCity());
+                pref.setSummary(getString(R.string.locationSet) + " " + address.getStreetname() + ", " + address.getCity());
             }
         }
     }
