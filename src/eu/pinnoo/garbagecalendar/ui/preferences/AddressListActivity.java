397e49d32f3f98bf7c35cc4426981770e22cde88
Wouter Pinnoo <pinnoo.wouter@gmail.com>
2013-08-05 17:06:51 +0200

    made searchview listen to queries and removed unused icons

--- src/eu/pinnoo/garbagecalendar/ui/preferences/AddressListActivity.java
+++ src/eu/pinnoo/garbagecalendar/ui/preferences/AddressListActivity.java
@@ -5,17 +5,10 @@ import android.app.SearchManager;
 import android.app.SearchableInfo;
 import android.content.Context;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.TextView.OnEditorActionListener;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
@@ -35,9 +28,8 @@ import java.util.List;
  *
  * @author Wouter Pinnoo <pinnoo.wouter@gmail.com>
  */
-public class AddressListActivity extends AbstractSherlockListActivity {
+public class AddressListActivity extends AbstractSherlockListActivity implements SearchView.OnQueryTextListener {
 
-    private EditText filterText;
     private List<Address> list;
     private AddressAdapter adapter;
 
@@ -46,52 +38,10 @@ public class AddressListActivity extends AbstractSherlockListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addresses);
 
-        final TextWatcher filterTextWatcher = new TextWatcher() {
-            public void afterTextChanged(Editable s) {
-            }
-
-            public void beforeTextChanged(CharSequence s, int start, int count,
-                    int after) {
-            }
-
-            public void onTextChanged(CharSequence s, int start, int before,
-                    int count) {
-                adapter.getFilter().filter(s);
-            }
-        };
-
         final ListView lv = getListView();
         lv.setOnItemClickListener(new OnItemClickListener() {
             public void onItemClick(AdapterView<?> av, View view, int i, long l) {
-                getSharedPreferences("PREFERENCE", Activity.MODE_PRIVATE)
-                        .edit()
-                        .putBoolean(LocalConstants.CacheName.COL_REFRESH_NEEDED.toString(), true)
-                        .commit();
-                UserData.getInstance().setAddress((Address) lv.getItemAtPosition(i));
-                finish();
-            }
-        });
-
-        filterText = (EditText) findViewById(R.id.search_box);
-        filterText.addTextChangedListener(filterTextWatcher);
-        filterText.setOnEditorActionListener(new OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE
-                        || (!event.isShiftPressed()
-                        && event.getKeyCode() == KeyEvent.KEYCODE_ENTER
-                        && event.getAction() == KeyEvent.ACTION_DOWN)) {
-                    if (lv.getChildCount() == 1) {
-                        getSharedPreferences("PREFERENCE", Activity.MODE_PRIVATE)
-                                .edit()
-                                .putBoolean(LocalConstants.CacheName.COL_REFRESH_NEEDED.toString(), true)
-                                .commit();
-                        UserData.getInstance().setAddress((Address) lv.getItemAtPosition(0));
-                        AddressListActivity.this.finish();
-                        return true;
-                    }
-                }
-                return false;
+                submitAddress(i);
             }
         });
     }
@@ -116,17 +66,42 @@ public class AddressListActivity extends AbstractSherlockListActivity {
         setListAdapter(adapter);
     }
 
+    public void submitAddress(int position) {
+        getSharedPreferences("PREFERENCE", Activity.MODE_PRIVATE)
+                .edit()
+                .putBoolean(LocalConstants.CacheName.COL_REFRESH_NEEDED.toString(), true)
+                .commit();
+        UserData.getInstance().setAddress((Address) getListView().getItemAtPosition(position));
+        finish();
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getSupportMenuInflater();
         inflater.inflate(R.menu.address_list_menu, menu);
-        
+
         MenuItem item = menu.findItem(R.id.address_search);
         SearchView searchView = (SearchView) item.getActionView();
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchableInfo info = searchManager.getSearchableInfo(getComponentName());
         searchView.setSearchableInfo(info);
-        
+        searchView.setOnQueryTextListener((SearchView.OnQueryTextListener) this);
+
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        final ListView lv = getListView();
+        if (lv.getChildCount() == 1) {
+            submitAddress(0);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        adapter.getFilter().filter(newText);
+        return true;
     }
 }
