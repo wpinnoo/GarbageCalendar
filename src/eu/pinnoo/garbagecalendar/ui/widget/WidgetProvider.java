67965c47ebc3dae45e52c5baba22426d0920c0b9
Wouter Pinnoo <pinnoo.wouter@gmail.com>
2014-02-11 23:07:45 +0100

    Changed copyright to 2014

--- src/eu/pinnoo/garbagecalendar/ui/widget/WidgetProvider.java
+++ src/eu/pinnoo/garbagecalendar/ui/widget/WidgetProvider.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2013 Wouter Pinnoo
+ * Copyright 2014 Wouter Pinnoo
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
