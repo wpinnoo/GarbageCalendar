7260a1a2750d066b57a82c244cd51a978e33aa00
Wouter Pinnoo <pinnoo.wouter@gmail.com>
2016-02-18 20:50:18 +0100

    Changed version codes to next version

--- app/src/main/java/eu/pinnoo/garbagecalendar/data/LocalConstants.java
+++ app/src/main/java/eu/pinnoo/garbagecalendar/data/LocalConstants.java
@@ -25,7 +25,7 @@ import java.text.SimpleDateFormat;
  */
 public class LocalConstants {
 
-    public static final boolean DEBUG = false;
+    public static final boolean DEBUG = true;
     public static final String ENCODING = "iso-8859-1";
     public static final String CALENDAR_URL = "https://raw.githubusercontent.com/wpinnoo/GarbageCalendar/master/extra/IvagoOphaalkalender2015.json";
     public static final String STREETS_URL = "https://raw.githubusercontent.com/wpinnoo/GarbageCalendar/master/extra/IVAGO-Stratenlijst.json";
